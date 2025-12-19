96f9536528d954d439cdf2ea63b74c54a53a9953
botfusion12-lang <botfusion12@gmail.com>
2025-12-19 19:25:26 +0530

    Delete backend/api/history.js

--- backend/api/history.js
+++
@@ -1,9 +0,0 @@
-import { db } from "../lib/db.js";
-
-export default function handler(req, res) {
-  const { userId } = req.query;
-
-  db.all(`SELECT * FROM chats WHERE user_id=?`, [userId],
-    (_, rows) => res.json(rows)
-  );
-}
