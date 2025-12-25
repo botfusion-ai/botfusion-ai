4f687b355832317d55721a0c0cdc2884b19643ff
Atharv Pawar <botfusion12@gmail.com>
2025-12-25 17:27:46 +0530

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,33 +1,18 @@
-const CACHE_NAME = 'botfusion-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/logo-192.png',
-  '/icons/logo-512.png'
-];
-
-// Install SW
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("botfusion-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js"
+      ]);
+    })
   );
 });
 
-// Activate SW
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(keyList.map(key => {
-        if(key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
-});
-
-// Fetch requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
